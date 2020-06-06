f5095a856926849ad5237bc277f1b9dfe0dbb82b
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-06-06 10:39:55 +0800

    adding license

--- setup.py
+++ setup.py
@@ -1,15 +1,47 @@
+'''
+MIT License
+
+Copyright (c) 2020 Jimeno A. Fonseca
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 from setuptools import setup
+import os
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+_version = {}
+_version_path = os.path.join(here, 'enthalpygradients', '__init__.py')
+with open(_version_path, 'r') as f:
+    exec(f.read(), _version)
 
 install_requires = ['numpy',
                     'pandas']
 
 setup(
     name='EnthalpyGradients',
-    version='1.0',
-    packages=['EnthalpyGradients'],
+    version=_version['__version__'],
+    packages=['enthalpygradients'],
     url='https://github.com/JIMENOFONSECA/EnthalpyGradients',
     license='MIT',
     author='Jimeno A. Fonseca',
