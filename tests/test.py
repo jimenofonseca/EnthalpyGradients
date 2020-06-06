d25f9d8247c75617c07263d7c313beda89f917e7
JIMENOFONSECA <fonseca@arch.ethz.ch>
2020-06-06 11:18:53 +0800

    make version 1.1 ready

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,7 @@
-import enthalpygradients
+from enthalpygradients import EnthalpyGradient
 
 
-
-c = enthalpygradients.EnthalpyGradients(18.5, 20)
+c = EnthalpyGradient(18.5, 20)
 r = c.enthalpy_gradient([23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34],
                           [23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34], 'total', 'hourly')
 l = c.specific_thermal_consumption([23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34,23,34,34,34],
